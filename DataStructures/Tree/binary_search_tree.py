75b720d87c44717fc89287d3bccedaad594bc706
NicolasOsorio2912 <micorreo@github.comn.osoriop@uniandes.edu.co>
2026-04-10 16:10:31 -0500

    'insert_node y put'

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,6 +1,9 @@
-def put():
-    pass
+from Tree import bst_node as bst
 
+def put(my_bst, key, value):
+    my_bst['root'] = bst.insert_node(my_bst['root'], key, value)
+    return my_bst
+    
 def get():
     pass
 
