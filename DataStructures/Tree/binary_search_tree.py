2ab02cc6b3bf6f4d27f39a6acdd36fd105030f75
jaimelesmes5 <ja.lesmes@uniandes.edu.co>
2026-04-10 16:26:09 -0500

    get()

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -4,8 +4,8 @@ def put(my_bst, key, value):
     my_bst['root'] = bst.insert_node(my_bst['root'], key, value)
     return my_bst
     
-def get():
-    pass
+def get(my_bst, key):
+    return bst.get_node(my_bst['root'], key)
 
 def size(my_bst):
     return bst.size_tree(my_bst['root'])
