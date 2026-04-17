b763d0fe711c41118cdb5427bdef9ccc7a0506e0
jaimelesmes5 <ja.lesmes@uniandes.edu.co>
2026-04-16 22:05:15 -0500

    binary_search_tree y bst_node

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,11 +1,62 @@
-from Tree import bst_node as bst
+from . import bst_node as bst
 
 def put(my_bst, key, value):
     my_bst['root'] = bst.insert_node(my_bst['root'], key, value)
+    my_bst['size'] = bst.size_tree(my_bst['root'])
     return my_bst
     
 def get(my_bst, key):
-    return bst.get_node(my_bst['root'], key)
+    node = bst.get_node(my_bst['root'], key)
+    return None if node is None else node['value']
 
 def size(my_bst):
     return bst.size_tree(my_bst['root'])
+
+def contains(tree, key):
+    return get(tree, key) is not None
+
+def is_empty(tree):
+    return tree["size"] == 0
+
+def key_set(tree):
+    result = []
+    bst.key_set_tree(tree["root"], result)
+    return result
+    
+def value_set(tree):
+    result = []
+    bst.value_set_tree(tree["root"], result)
+    return result
+    
+def get_min(tree):
+    node = bst.get_min_node(tree["root"])
+    return None if node is None else node["key"]
+
+
+def get_max(tree):
+    node = bst.get_max_node(tree["root"])
+    return None if node is None else node["key"]
+
+
+def delete_min(tree):
+    if tree["root"] is not None:
+        tree["root"] = bst.delete_min_tree(tree["root"])
+        tree["size"] -= 1
+
+def delete_max(tree):
+    if tree["root"] is not None:
+        tree["root"] = bst.delete_max_tree(tree["root"])
+        tree["size"] -= 1
+        
+def height(tree):
+    return bst.height_tree(tree["root"])
+
+def keys(tree, low, high):
+    result = []
+    bst.keys_range(tree["root"], result, low, high)
+    return result
+
+def values(tree, low, high):
+    result = []
+    bst.values_range(tree["root"], result, low, high)
+    return result
--- DataStructures/Tree/bst_node.py
+++ DataStructures/Tree/bst_node.py
@@ -95,7 +95,7 @@ def get_node(root, key):
         return get_node(root['right'], key)
         
     else: 
-        return root['value']      
+        return root   
 
 def size_tree(root):
     
@@ -104,3 +104,101 @@ def size_tree(root):
     
     return root['size']
 
+def height_tree(root):
+    if root is None:
+        return -1
+    
+    left_h = height_tree(root['left'])
+    right_h = height_tree(root['right'])
+    
+    return 1 + max(left_h, right_h)
+
+def get_min_node(root):
+    if root is None:
+        return None
+    
+    if root['left'] is None:
+        return root
+    
+    return get_min_node(root['left'])
+
+def get_max_node(root):
+    if root is None:
+        return None
+    
+    if root['right'] is None:
+        return root
+    
+    return get_max_node(root['right'])
+
+def delete_min_tree(root):
+    if root is None:
+        return None
+    
+    if root['left'] is None:
+        return root['right']
+    
+    root['left'] = delete_min_tree(root['left'])
+    
+    left_size = root['left']['size'] if root['left'] else 0
+    right_size = root['right']['size'] if root['right'] else 0
+    root['size'] = 1 + left_size + right_size
+    
+    return root
+
+def delete_max_tree(root):
+    if root is None:
+        return None
+    
+    if root['right'] is None:
+        return root['left']
+    
+    root['right'] = delete_max_tree(root['right'])
+    
+    left_size = root['left']['size'] if root['left'] else 0
+    right_size = root['right']['size'] if root['right'] else 0
+    root['size'] = 1 + left_size + right_size
+    
+    return root
+
+def key_set_tree(root, result):
+    if root is None:
+        return
+    
+    key_set_tree(root['left'], result)
+    result.append(root['key'])
+    key_set_tree(root['right'], result)
+    
+def value_set_tree(root, result):
+    if root is None:
+        return
+    
+    value_set_tree(root['left'], result)
+    result.append(root['value'])
+    value_set_tree(root['right'], result)
+    
+def keys_range(root, result, low, high):
+    if root is None:
+        return
+    
+    if low < root['key']:
+        keys_range(root['left'], result, low, high)
+    
+    if low <= root['key'] <= high:
+        result.append(root['key'])
+    
+    if high > root['key']:
+        keys_range(root['right'], result, low, high)
+
+def values_range(root, result, low, high):
+    if root is None:
+        return
+    
+    if low < root['key']:
+        values_range(root['left'], result, low, high)
+    
+    if low <= root['key'] <= high:
+        result.append(root['value'])
+    
+    if high > root['key']:
+        values_range(root['right'], result, low, high)
